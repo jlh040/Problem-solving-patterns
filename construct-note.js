a565392666d3d50ae552b6d5210cc8ef942135ef
Jeff Hall <jeff.leon.hall@gmail.com>
2021-09-24 20:30:07 -0700

    implemented constructNote

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,22 @@
-// add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(message, letters) {
+  const messageFreqObj = {};
+  const lettersFreqObj = {};
+
+  for (let char of message) {
+    messageFreqObj[char] = messageFreqObj[char] + 1 || 1;
+  }
+  for (let char of letters) {
+    lettersFreqObj[char] = lettersFreqObj[char] + 1 || 1;
+  }
+
+  // loop over the message object
+  for (let char in messageFreqObj) {
+    if (lettersFreqObj[char] >= messageFreqObj[char]) {
+      null;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
 }
