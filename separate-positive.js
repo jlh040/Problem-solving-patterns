c850171da2e48eb6ba587b4925740e63b8db9c7d
Jeff Hall <jeff.leon.hall@gmail.com>
2021-09-25 12:18:49 -0700

    implemented separatePositive

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,23 @@
-// add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(arr) {
+  let left = 0;
+  let right = 1;
+
+  while (right < arr.length) {
+    if (arr[left] < 0) {
+      if (arr[right] > 0) {
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right++;
+      } else {
+        right++;
+      }
+    } else {
+      left++;
+      right++;
+    }
+  }
+
+  return arr;
 }
