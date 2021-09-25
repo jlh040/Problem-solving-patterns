0138f9661386bb7555f7980f6f617a46d89dfb71
Jeff Hall <jeff.leon.hall@gmail.com>
2021-09-25 10:42:30 -0700

    implemented the averagePair function

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,16 @@
-// add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(arr, targetAverage) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    if ((arr[left] + arr[right]) / 2 === targetAverage) {
+      return true;
+    } else if ((arr[left] + arr[right]) / 2 < targetAverage) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return false;
 }
