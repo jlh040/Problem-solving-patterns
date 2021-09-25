f3f2bf4025d0f219ef85e2b3814af45280dd9853
Jeff Hall <jeff.leon.hall@gmail.com>
2021-09-25 12:45:05 -0700

    implemented isSubsequence

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,13 @@
-// add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(str1, str2) {
+  let pointer = 0;
+  for (let i = 0; i < str2.length; i++) {
+    if (str2[i] === str1[pointer]) {
+      pointer++;
+    }
+    if (pointer === str1.length) {
+      return true;
+    }
+  }
+
+  return false;
 }
