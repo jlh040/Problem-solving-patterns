c6db45e16c1e292084a633b95eb152f75ccf907a
Jeff Hall <jeff.leon.hall@gmail.com>
2021-09-25 11:07:46 -0700

    implemented the twoArrayObject function

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,13 @@
-// add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keyArr, valueArr) {
+  const output = {};
+
+  for (let i = 0; i < keyArr.length; i++) {
+    if (valueArr[i] === undefined) {
+      output[keyArr[i]] = null;
+    } else {
+      output[keyArr[i]] = valueArr[i];
+    }
+  }
+
+  return output;
 }
