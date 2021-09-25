7ef12c924f525318c3e218ab815e093ec044fc3e
Jeff Hall <jeff.leon.hall@gmail.com>
2021-09-25 11:36:02 -0700

    implemented the sameFrequency function

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,25 @@
-// add whatever parameters you deem necessary
-function sameFrequency() {
+function sameFrequency(num1, num2) {
+  const num1String = num1.toString();
+  const num2String = num2.toString();
+  const num1StringFreqObj = {};
+  const num2StringFreqObj = {};
+
+  if (num1String.length !== num2String.length) return false;
+
+  for (let char of num1String) {
+    num1StringFreqObj[char] = num1StringFreqObj[char] + 1 || 1;
+  }
+  for (let char of num2String) {
+    num2StringFreqObj[char] = num2StringFreqObj[char] + 1 || 1;
+  }
+
+  for (let char in num1StringFreqObj) {
+    if (!num2StringFreqObj[char]) {
+      return false;
+    } else if (num1StringFreqObj[char] !== num2StringFreqObj[char]) {
+      return false;
+    }
+  }
+
+  return true;
 }
